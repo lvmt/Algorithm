93a3a093fb3d98e0380585b3b57e3183e6592bca
lvmt <13554221497@163.com>
2020-11-08 12:43:05 +0800

    快速排序

---
+++ 快速排序/demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i < pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        
+        return quicksort(less) + [pivot] + quicksort(greater)
+    
+print(quicksort([19, 10, 5, 2, 3]))
+
