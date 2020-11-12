9c9404b873fd9e591756b7fd2dba4e31a7296379
lvmt <13554221497@163.com>
2020-11-12 18:59:53 +0800

    水仙花数

---
+++ 水仙花数/demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+for num in range(100, 1000):
+    low = num % 10   
+    mid = num // 10 % 10
+    high = num // 100
+    if num == low ** 3 + mid ** 3 + high ** 3:
+        print(num) 
+
+
+'''
+% 取余
+// 整除
+
+'''
