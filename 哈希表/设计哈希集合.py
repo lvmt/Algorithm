240b9819f4f1d84a97d235e6a2dfdd77573d02fc
lvmt <13554221497@163.com>
2021-02-03 07:17:17 +0800

    ll

--- 哈希表/设计哈希集合.py
+++ 哈希表/设计哈希集合.py
@@ -2,3 +2,20 @@
 # -*- coding:utf-8 -*-
 # @Time 2021/1/29 14:29
 # @File 设计哈希集合.py 
+
+
+import  argparse
+from collections import defaultdict
+from collections import defaultdict
+
+def sum(x, y):
+    """
+    Args:
+        x ([type]): [description]
+        y ([type]): [description]
+    """
+    return x + y
+
+
+
+
